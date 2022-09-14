a567ce826a257fb2a9a389cc8b286fd4270102d6
Emmanuel <endiesworld@gmail.com>
2022-09-14 05:51:04 +0100

    updated server

--- server_projects/basic_http_server.js
+++ server_projects/basic_http_server.js
@@ -1,7 +1,23 @@
 const http = require('http')
+const url = require('url')
 
-const server = http.createServer((req, res)=>{
-    console.log(req)
+const person = {
+    '1' : {
+        id : '1',
+        name: 'ndubuisi',
+        age: 35,
+        sex: 'male'
+    }
+}
+
+const server = http.createServer()
+server.on('request',(req, res)=>{
+    const parsedurl = url.parse(req.url, true)
+    if(req.method === 'GET' && parsedurl.pathname === '/ndubuisi'){
+        const {id} = parsedurl.query
+        console.log(id, typeof(id))
+        console.log(person[id])
+    }
 })
 
 server.listen(3000)
