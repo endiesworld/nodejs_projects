ec68ed533773b2a55e6f0c89d0d967be2f5cfe14
endiesworld <endiesworld@gmail.com>
2022-09-21 00:34:58 +0100

    Using exprssjs

--- server_projects/using_express/app.js
+++ server_projects/using_express/app.js
@@ -1,9 +1,19 @@
-const http = require('http')
+// const http = require('http')  // No need for this any more because we have same from Expressjs
 
 const express = require('express')
 
 const app = express()
 
-const server = http.createServer(app)
+app.use((req, res, next)=>{
+    console.log('In the first middleware')
+    next()
+})
 
-server.listen(3000)
+app.use((req, res, next)=>{
+    console.log('In the second middleware')
+    res.send('<h1> Hello from')
+})
+
+// const server = http.createServer(app)
+
+app.listen(3000)
